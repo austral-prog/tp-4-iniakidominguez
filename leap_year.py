8a42c05efb66bcb991af410c4701a608de6a1178
iniakidominguez <73805462+iniakidominguez@users.noreply.github.com>
2025-04-15 09:43:00 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,18 @@
 def leap_year():
-    print("TO DO")
+    text=input("ingrese el año: ")
+    year=int(text)
+    resto=year%4
+    cent=year%100
+    cuat=year%400
+    if resto==0:
+        if cent==0:
+            if cuat==0:
+                print(f"El año {year} es bisiesto")
+            else:
+                print(f"el año {year} no es bisiesto")
+            else: 
+                print(f"El año {year} es bisiesto")
+            else:
+                 print(f"el año {year} no es bisiesto")
+leap_year()
+
